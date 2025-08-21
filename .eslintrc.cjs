5517af65454f542abbe8177a5cbe9e7b1d346aec
Julia Nowicka <julia.nowicka@lemondev.pl>
2025-08-21 13:48:25 +0200

    chore(lint): add ESLint + Prettier configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      typescript: true,
+    },
+  },
+  env: { browser: true, es2023: true, node: true },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+      }
+    ],
+  },
+};
